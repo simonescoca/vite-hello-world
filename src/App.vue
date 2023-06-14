d1a05ef4a9b2e145dd8a4e7d46bec79b39c018e6
simonescoca <simone.scoca@icloud.com>
2023-06-14 17:38:43 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+    import Title from './components/AppTitle.vue'
+    export default {
+        components: {
+            Title
+        },
+        data () {
+            return {
+            message: 'Welcome to my Vue app!'
+            }
+        }
+    }
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+	export default {
+		data () {
+			return {
+				message: "La mia prima app con Vite!"
+			}
+		}
+	}
+</script>
+
+<template>
+	<h1>
+		{{message}}
+	</h1>
+</template>
+
+<style scoped>
+</style>
